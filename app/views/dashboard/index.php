8df3bf0b865b97eb43ae95c85071a25505a0e5cf
shafaarika <arikanindi513@gmail.com>
2021-06-17 16:21:27 +0700

    update

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -29,10 +29,10 @@
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
 
-                    <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mt-5 text-gray-800">Dashboard</h1>
-                    </div>
+                <!-- Page Heading -->
+                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 class="h3 mt-5 text-gray-800">Dashboard</h1>
+                </div>
                 </div>
 
                 <div class="row">

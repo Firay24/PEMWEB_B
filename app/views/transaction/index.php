9d4e45d93928fc551eb9cc8cf70f639b5fe1cefb
firay24 <72513085+Firay24@users.noreply.github.com>
2021-06-18 03:02:54 +0700

    transaksi

---
+++ app/views/transaction/index.php
@@ -0,0 +1,42 @@
+ <!-- Begin Page Content -->
+ <div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mt-5 text-gray-800">Transaction</h1>
+    </div>
+
+    <!-- Body Conten -->
+    <div class="container-fluid">
+
+        <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Transaction History</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Class</th>
+                                <th>Price</th>
+                                <th>Datetime</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Pemograman berbasis website menggunakan Laravel</td>
+                                <td>Rp 200000</td>
+                                <td>2021-06-15 00:21:53.000000</td>
+                                <td><a href="" class="badge badge-warning">Belum Bayar</a></td>
+                            </tr>
+                            <tr>
+                                <td>Database dengan My SQL</td>
+                                <td>Rp 150000</td>
+                                <td>2021-06-16 00:21:53.000000</td>
+                                <td><a href="" class="badge badge-success">Lunas</a></td>
+                            </tr>
+                        </tbody>
+</div>

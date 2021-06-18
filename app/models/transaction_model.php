ce09ba8a42e207d2b647da6ad91a31032b11fe0d
firay24 <72513085+Firay24@users.noreply.github.com>
2021-06-18 15:58:18 +0700

    up

--- app/models/transaction_model.php
+++ app/models/transaction_model.php
@@ -10,8 +10,18 @@ class transaction_model extends Controller{
     }
 
     public function getAll() {
-        $this->db->query('SELECT Total, Waktu, Keterangan,judulKelas FROM transaksi INNER JOIN kelas ON transaksi.IDKelas=kelas.ID');
+        $this->db->query('SELECT kelas.ID, Total, Waktu, Keterangan, judulKelas FROM transaksi INNER JOIN kelas ON transaksi.IDKelas=kelas.ID');
         return $this->db->resultSet();
+    }
+
+    public function deleteTransaksi($id) {
+        $query = "DELETE FROM transaksi WHERE ID = :id";
+        $this->db->query($query);
+        $this->db->bind('id', $id);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
     }
     
 }
